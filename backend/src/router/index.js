8bc752c8fa293da3c0e4dc59063a4574024c9e78
Djok64 <joakym.anc@gmail.com>
2023-10-28 20:34:53 +0200

    creation du middleware authorization  qui permet de savoir si un utilisateur est connecter ou non grace a lobtention du token suite a ça connexion lors d'un acces a une route qui necessite d'être connecter le middleware vérifié en decryptant le token voir si il correspond bien a la phrase du process.env.SECRET_TOKEN voir fichier auth.js

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -11,14 +11,16 @@ const commandeRoutes = require("./commandeRoutes");
 
 const authRoutes = require("./authRoutes");
 
+const authorization = require("../middleware/auth");
+
 router.get("/", (req, res) => {
   res.status(200).send("on est la dans /api sur index.js du dossier routeur");
 });
 
 router.use("/games", gameRoutes);
-router.use("/users", userRoutes);
-router.use("/clients", clientRoutes);
-router.use("/commandes", commandeRoutes);
+router.use("/users", authorization, userRoutes);
+router.use("/clients", authorization, clientRoutes);
+router.use("/commandes", authorization, commandeRoutes);
 router.use("/auth", authRoutes);
 
 module.exports = router;
